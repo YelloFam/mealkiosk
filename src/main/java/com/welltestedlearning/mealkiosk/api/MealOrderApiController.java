535f73620e4086dab850855b06d17f95e9678f2e
Ted M. Young <tedyoung@gmail.com>
2018-09-12 08:46:24 -0500

    Part II - Lab 5, B & C (this completes Lab 5)

--- src/main/java/com/welltestedlearning/mealkiosk/api/MealOrderApiController.java
+++ src/main/java/com/welltestedlearning/mealkiosk/api/MealOrderApiController.java
@@ -3,6 +3,7 @@ package com.welltestedlearning.mealkiosk.api;
 import com.welltestedlearning.mealkiosk.adapter.MealBuilder;
 import com.welltestedlearning.mealkiosk.domain.MealOrder;
 import com.welltestedlearning.mealkiosk.domain.MealOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,8 @@ public class MealOrderApiController {
   public MealOrderApiController() {
   }
 
-  // This constructor supports "dependency injection": passing in the repo for the controller to use
+  @Autowired  // if we keep the above default constructor,
+  // @Autowired tells Spring to use this constructor
   public MealOrderApiController(MealOrderRepository mealOrderRepository) {
     this.mealOrderRepository = mealOrderRepository;
   }
@@ -31,10 +33,11 @@ public class MealOrderApiController {
 
     int price = mealOrder.price();
 
-    mealOrderRepository.save(mealOrder);
+    MealOrder savedMealOrder = mealOrderRepository.save(mealOrder);
 
     MealOrderResponse mealOrderResponse = new MealOrderResponse();
     mealOrderResponse.setPrice(price);
+    mealOrderResponse.setId(savedMealOrder.getId().toString());
 
     return mealOrderResponse;
   }
--- src/main/java/com/welltestedlearning/mealkiosk/domain/MealOrderRepository.java
+++ src/main/java/com/welltestedlearning/mealkiosk/domain/MealOrderRepository.java
@@ -1,11 +1,14 @@
 package com.welltestedlearning.mealkiosk.domain;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class MealOrderRepository {
 
   private Map<Long, MealOrder> mealOrderMap = new HashMap<>();
