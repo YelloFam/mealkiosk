e54eebfaf443a88d1d5b14ee431a5f3cd177a884
Ted M. Young <tedyoung@gmail.com>
2018-09-12 08:41:06 -0500

    Part II - Lab 5A: added ID propertyˆ

--- src/main/java/com/welltestedlearning/mealkiosk/api/MealOrderResponse.java
+++ src/main/java/com/welltestedlearning/mealkiosk/api/MealOrderResponse.java
@@ -2,6 +2,15 @@ package com.welltestedlearning.mealkiosk.api;
 
 public class MealOrderResponse {
   private int price;
+  private String id;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 
   public int getPrice() {
     return price;
